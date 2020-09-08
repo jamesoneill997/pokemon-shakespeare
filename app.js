80e37761ca55521ddb04d5086c568343a2803932
jamesoneill997 <jamesoneill997@gmail.com>
2020-09-08 22:43:55 +0100

    refactored getDescription and commented

--- app.js
+++ app.js
@@ -1,3 +1,12 @@
 const express = require('express')
-const {getDesc} = require('./src/getDescription')
+const getDescription = require('./src/getDescription')
+
+const desc = getDescription("bulbasaur", (error, response)=>{
+    if(error){
+        return console.log(error, 'Error fetching pokemon description.')
+    }
+    else{
+        return console.log(response)
+    }   
+})
 
--- src/getDescription.js
+++ src/getDescription.js
@@ -1,18 +1,24 @@
 const request = require('postman-request')
 
+//getDescription takes a pokemon name as a parameter and returns an English description
 const getDescription = (pokeName, callback)=>{
+    //create API query
     const url = 'https://pokeapi.co/api/v2/pokemon-species/' + pokeName
     
+    //send request to pokeapi
     request({url, json:true}, (error, {body})=>{
         const {err} = body
+        //error handling
         if(error){
             callback('Error connecting with weather service.', undefined)
         }
         else if(err){
             callback(err.info, undefined)
         }
+
+        //send back description (english version)
         else{
-            callback(undefined, body)
+            callback(undefined, body['flavor_text_entries'][0]['flavor_text'])
         }
     })
 
