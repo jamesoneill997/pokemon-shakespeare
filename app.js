71fbea6aaad0cf74272229dfaa5173caf3c97077
jamesoneill997 <jamesoneill997@gmail.com>
2020-09-08 22:49:01 +0100

    setup root endpoint with express

--- app.js
+++ app.js
@@ -1,12 +1,36 @@
 const express = require('express')
 const getDescription = require('./src/getDescription')
 
-const desc = getDescription("bulbasaur", (error, response)=>{
-    if(error){
-        return console.log(error, 'Error fetching pokemon description.')
-    }
-    else{
-        return console.log(response)
-    }   
+
+const app = express()
+const port = process.env.PORT || 3000
+
+//setup endpoints
+app.get('/',(req, res) => {
+    res.send('Please provide a pokemon name :)')    
 })
 
+
+app.get('*', (req,res)=>{
+    
+})
+
+
+
+
+
+
+
+//-------------------Test Call------------------------------------
+// const desc = getDescription("bulbasaur", (error, response)=>{
+//     if(error){
+//         return console.log(error, 'Error fetching pokemon description.')
+//     }
+//     else{
+//         return console.log(response)
+//     }   
+// })
+
+app.listen(port, ()=>{
+    console.log('Server running on port '+port)
+})
