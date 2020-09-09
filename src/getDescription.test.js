27cf9c7807448015458ccaa3ff240224deddba96
jamesoneill997 <jamesoneill997@gmail.com>
2020-09-09 10:13:07 +0100

    added test files for description & translation

---
+++ src/getDescription.test.js
@@ -0,0 +1,16 @@
+const {getDescription} = require('./getDescription')
+
+it("Should return pokemon description",()=>{
+    let desc = getDescription("Bulbasaur", (err, description)=>{
+        if(err){
+            return err
+        }
+        else{
+            return description
+        }
+    })
+
+    if(desc != "A strange seed was\nplanted on its\nback at birth.\fThe plant sprouts\nand grows with\nthis POKéMON."){
+        throw new Error('Expected "A strange seed was\nplanted on its\nback at birth.\fThe plant sprouts\nand grows with\nthis POKéMON.", but got '+desc)
+    }
+})
---
+++ src/getTranslation.test.js
@@ -0,0 +1,16 @@
+const {getTranslation} = require('./getTranslation')
+
+it("Should translate English to Shakespearean English",()=>{
+    let greeting = getTranslation("Hello,there. How are you today?", (err, translation)=>{
+        if(err){
+            return err
+        }
+        else{
+            return translation
+        }
+    })
+
+    if(greeting != "Valorous morrow to thee, sir, thither. How art thee the present day?"){
+        throw new Error('Expected "Valorous morrow to thee, sir, thither. How art thee the present day?", but got '+greeting)
+    }
+})
